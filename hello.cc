e31a74156672c1740fafc63fbbe72a0d51ee1312
Artur Bandaryk <arthur_bandaryk@mail.ru>
2021-12-02 14:15:10 +0300

    2

--- hello.cc
+++ hello.cc
@@ -2,12 +2,8 @@
 
 using namespace std;
 
-<<<<<<< HEAD
-int main(int argc, char** argv) {
-=======
 // Just print 'Hello!'.
-int main(int argc, char** argv){
->>>>>>> 606a982f244b55da1598153554871a015fa9e926
+int main(int argc, char** argv) {
   cout << "Hello!" << endl;
   return cout.good() ? EXIT_SUCCESS : EXIT_FAILURE;
 }
